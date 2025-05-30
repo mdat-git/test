bc51a781106574a4d13721fb2c64eebff517d8ac
mdat-git <52555642+mdat-git@users.noreply.github.com>
2025-05-30 15:22:23 -0700

    Update ctestmi.py

--- ctestmi.py
+++ ctestmi.py
@@ -1,17 +1,5 @@
 let
     // STEP 1: Connect to SharePoint and find your file
-    Source = SharePoint.Files("https://edisonintl.sharepoint.com/teams/GridOperationsAnalyticsandTech-TD/", [ApiVersion = 15]),
-    FilteredFiles = Table.SelectRows(Source, each Text.StartsWith([Folder Path], "https://edisonintl.sharepoint.com/teams/GridOperationsAnalyticsandTech-TD/Projects/Validation CMI Performance") and [Name] = "validation_metrics_dataset.csv"),
-    VisibleFile = Table.SelectRows(FilteredFiles, each [Attributes][Hidden]? <> true),
 
-    // STEP 2: Grab the file content from the first row
-    FileContent = VisibleFile{0}[Content],
-
-    // STEP 3: Read the CSV and promote headers dynamically
-    CSVTable = Csv.Document(FileContent, [Delimiter = ",", Encoding = 65001, QuoteStyle = QuoteStyle.None]),
-    Promoted = Table.PromoteHeaders(CSVTable, [PromoteAllScalars = true]),
-
-    // STEP 4: Automatically detect and apply types
-    AutoTyped = Table.AutoDetectColumnTypes(Promoted)
 in
     AutoTyped
