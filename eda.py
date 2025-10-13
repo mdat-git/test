d1928736d21c191a6da5df99c544050128bc2bed
mdat-git <52555642+mdat-git@users.noreply.github.com>
2025-10-13 15:16:44 -0700

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,15 +1,12 @@
 import re
 
 PAT_ENERGIZED_SET = re.compile(r"""
-(?ix)
-\b(?:his\s+)?location      # 'Location' or 'His Location'
-\s*\[\s*(?P<loc_id>\d+)\s*\]   # [11111111111]
-\s*energized\s*date\s*has\s*been\s*set\s*to
-\s*\[\s*(?P<dt>              # capture the datetime
-    (?:\d{1,2}/\d{1,2}/\d{2,4})      # MM/DD/YYYY (or M/D/YY)
-    \s+\d{1,2}:\d{2}:\d{2}           # HH:MM:SS (24h or with AM/PM below)
-    (?:\s?(?:AM|PM))?                # optional AM/PM
-    |                                # OR ISO-ish
-    \d{4}-\d{2}-\d{2}[ T]\d{2}:\d{2}:\d{2}
+\b(?:his\s+)?location              # 'Location' or 'His Location'
+\s*\[\s*(?P<loc_id>\d+)\s*\]       # [11111111111]
+\s*energized\s*date\s*has\s*been\s*(?:set|updated)\s*to
+\s*\[\s*(?P<dt>                    # capture the datetime
+    (?:\d{1,2}/\d{1,2}/\d{2,4} | \d{4}-\d{2}-\d{2})
+    \s+\d{1,2}:\d{2}:\d{2}
+    (?:\s?(?:AM|PM))?
 )\s*\]
-""")
+""", re.IGNORECASE | re.VERBOSE)
